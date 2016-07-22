acc9c959680e38a58a52a8bace7d85fb04db6fcf
sr <skyrousse@gmail.com>
2016-07-22 11:10:09 -0700

    add validation for presence of a name and length restriction for Store class

--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,5 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many(:brands)
+  validates(:name, {:presence => true, :length => { :maximum => 20}})
 
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -5,4 +5,8 @@ describe(Store) do
     test_store = Store.new({:name => ""})
     expect(test_store.save()).to(eq(false))
   end
+  it('restricts the length of a name to 20 characters or less') do
+    test_store = Store.new({:name => "a".*(21)})
+    expect(test_store.save()).to(eq(false))
+  end
 end
