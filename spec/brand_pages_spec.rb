de309b88e07ad0f02fbd48a5d6fb60c5293101b6
sr <skyrousse@gmail.com>
2016-07-25 14:18:23 -0700

    files pulled from origin to be commited with latest

--- spec/brand_pages_spec.rb
+++ spec/brand_pages_spec.rb
@@ -1,12 +1,1 @@
-require('spec_helper')
 
-describe('add a brand to a store route', {:type => :feature}) do
-  it('allows a user to add a specific brand to a store') do
-    test_store = Store.create({:name => "stumpys"})
-    visit('/stores')
-    click_link('Stumpys')
-    fill_in('brand_name', :with => "test_brand")
-    click_button('Add Brand')
-    expect(page).to have_content("Test_brand")
-  end
-end
