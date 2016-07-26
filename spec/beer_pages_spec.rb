6d25c26bc8c50eff484213907c2810e52303b25c
sr <skyrousse@gmail.com>
2016-07-26 11:43:10 -0700

    add variables that will return more info about a beer

--- spec/beer_pages_spec.rb
+++ spec/beer_pages_spec.rb
@@ -4,7 +4,7 @@ describe('find a beer path', {:type => :feature}) do
   it('allows a user to find a beer by name') do
     visit('/')
     fill_in("beer_name", :with => "Aztec")
-    click_button('beer_search.btn')
+    click_button('beer_search')
     expect(page).to have_content("Aztec")
   end
 end
