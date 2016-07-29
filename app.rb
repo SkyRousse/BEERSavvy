0c0f27fcbd332b941c27aaea284505d7bdd97801
James Turello <jturello@yahoo.com>
2016-07-28 20:08:29 -0700

    hit 400 request limit on 3 keys i know - still debugging page blow ups

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file}
 enable :sessions
 
 brewery_db = BreweryDB::Client.new do |config|
-  config.api_key = ("7ad2c83100277e6fe89592046aae718c")
+  config.api_key = ("37f05932e468ea014afabb1d166a6f99")
 end
 
 get('/') do
@@ -19,11 +19,29 @@ end
 
 get("/beers/:name") do
   @all_beers = []
-  @name = params.fetch('name').gsub('+', ' ')
+
+  no_apos_name = params[:name].strip.gsub "'", ""
+  @name = no_apos_name.strip.gsub(/ /,'+')
+
+binding.pry
+
+  # @name = params['name'].gsub('+', ' ')
   @all_beers.push(brewery_db.beers.all(name: @name, withBreweries: 'Y').first)
-  if @all_beers.first.nil?
-    flash[:error] = 'No data returned. Try another search.'
-    redirect('/')
+
+
+puts @all_beers.first.nil?
+puts !([:breweries, :description, :name_display, :style].all? {|key| @all_beers.first.has_key? key})
+puts !([:id, :srm_min, :srm_max, :short_name, :description].all? {|key| @all_beers.first[:style].has_key? key})
+puts @all_beers.first
+
+binding.pry
+
+  if @all_beers.first.nil? ||
+    !([:breweries, :description, :name_display, :style ].all? {|key| @all_beers.first.has_key? key}) ||
+    !([:id, :srm_min, :srm_max, :short_name, :description].all? {|key| @all_beers.first[:style].has_key? key})
+
+      flash[:error] = 'No data returned. Try another search.'
+      redirect('/')
   else
     @brewery = @all_beers[0][:breweries]
     @style_id = @all_beers[0][:style][:id]
@@ -58,39 +76,50 @@ get('/breweries/:id') do
   @brewery = brewery_db.breweries.find(@id)
   @beers = brewery_db.brewery(@id).beers(withBreweries: 'Y')
   @beers.each_with_index do |item, index|
+
     if [:description, :name_display, :style, :ibu, :abv].all? {|key| item.has_key? key}
       if [:id, :srm_min, :srm_max, :short_name, :description].all? {|key| item[:style].has_key? key}
         @all_beers.push(item)
       end
     end
   end
-# binding.pry
-  # need to find correct values for individual beers
-  @srm_min = @all_beers[0][:style][:srm_min]
-  @srm_max = @all_beers[0][:style][:srm_max]
-  @srm_avg = (@srm_min.to_i + @srm_max.to_i)/2
 
+  # need to find correct values for individual beers
+  begin
+    @srm_min = @all_beers[0][:style][:srm_min]
+    @srm_max = @all_beers[0][:style][:srm_max]
+    @srm_avg = (@srm_min.to_i + @srm_max.to_i)/2
+  rescue
+    @srm_avg = 3/2
+  end
   erb(:brewery)
 end
 
 post('/random') do
-  count = 1
-  begin
+  count = 0
+  until count == 3
     returned = brewery_db.beers.random
-    # binding.pry
-    # puts "top - returned[:name] - #{returned[:name]}"
-    if ((returned[:name] =~ /\W /).nil?)
-      @name = returned[:name].strip.gsub(/ /,'+')
+
+puts "count: #{count.to_s}"
+puts "top - returned[:name] - #{returned[:name]}"
+
+    if ((returned[:name] =~ /[^ -~] /).nil?)
+      no_apos_name = returned[:name].strip.gsub "'", ""
+      @name = no_apos_name.strip.gsub(/ /,'+')
+
+puts "returned[:name] - " + returned[:name]
+puts ((returned[:name] =~ /[^ -~] /).nil?)
+
       break
     end
     count += 1
-    # puts count.to_s
-  end until count == 3
+  end
+
+puts "after if - @name: #{@name}"
+
   if !(@name.nil?)
-    # puts "/beers/#{@name}"
     redirect("/beers/#{@name}")
   else
-    # puts "/" + "returned[:name] - #{returned[:name]}"
     redirect '/'
   end
 end
